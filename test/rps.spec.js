16feed3b68c2d21228a75562d88f732959d2713c
unknown <fanglu0805@gmail.com>
2022-04-13 21:02:03 -0400

    added a folder?

---
+++ test/rps.spec.js
@@ -0,0 +1,33 @@
+import { player1 } from "../rps";
+import { hands } from "../rps";
+import { getHand } from "../rps";
+import { playRound } from "../rps"
+import {jest} from '@jest/globals';
+// "toBe()"
+test("Player 1's name is Kobe", () => {
+    expect(player1.name).toBe('Kobe')
+})
+
+// "toHaveLength()"
+test("how many hands can be played?", () => {
+    expect(hands).toHaveLength(3)
+})
+
+// "toBeTruthy()"
+test("there is 3 possible hands to play", () => {
+    expect(hands).toBeTruthy()
+})
+
+// toHaveBeenCalled()
+test("rock is a hand", () => {
+    const hand = jest.fn();
+    getHand(hand, 'rock');
+    expect(hand).not.toHaveBeenCalled()
+})
+
+// toHaveReturned()
+test("getHand returns a hand", () => {
+    const hand = jest.fn(() => true)
+    hand();
+    expect(hand).toHaveReturned();
+})
